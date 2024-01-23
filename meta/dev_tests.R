e561498aa6a170ea181ffa16c7b08063e7d732a8
Shawn O'Neil <oneilsh@gmail.com>
2024-01-22 22:36:57 -0800

    connection test with neo2r

--- meta/dev_tests.R
+++ meta/dev_tests.R
@@ -7,3 +7,26 @@ monarch_search("syndrome", limit = 5) %>%
 
 monarch_search("syndrome", limit = 30)
 monarch_search("syndrome", limit = 20)
+
+
+library(neo2R)
+graph <- startGraph(
+	"http://24.144.94.219:7474",
+	#"http://neo4j-bolt.monarchinitiative.org:7474"
+	#username="neo4j", password="donttrustusers",
+	#importPath="~/neo4j_home/neo4jImport",
+	#.opts = list(ssl_verifypeer=0)
+
+)
+
+df <- cypher(
+	graph,
+	prepCql(
+		'MATCH (n)-[r]-(p) return n,r,p limit 2000'
+	),
+	result = "graph"
+)
+
+
+
+
