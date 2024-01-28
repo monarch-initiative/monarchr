f0a7a592e204858bcc2868baf67d310c6b7d3cb4
Shawn O'Neil <oneilsh@gmail.com>
2024-01-27 22:23:18 -0800

    neo4j query -> kgx-compatible tidygraph

--- meta/dev_tests.R
+++ meta/dev_tests.R
@@ -1,87 +1,21 @@
-library(monarchr)
 library(tidygraph)
-
-monarch_search("syndrome", limit = 5) %>%
-    activate(nodes) %>%
-    filter(label == "EEC syndrome and related syndrome")
-
-monarch_search("syndrome", limit = 30)
-monarch_search("syndrome", limit = 20)
-
-
 library(neo2R)
-graph <- startGraph(
-	"http://24.144.94.219:7474",
-	#"http://neo4j-bolt.monarchinitiative.org:7474"
-	#username="neo4j", password="donttrustusers",
-	#importPath="~/neo4j_home/neo4jImport",
-	#.opts = list(ssl_verifypeer=0)
+library(dotenv)
 
-)
+load_dot_env()
 
-system.time({
-	df <- cypher(
-		graph,
-		prepCql(
-			'MATCH (n)-[r]-(p) return n,r,p limit 1'
-		),
-		result = "graph"
-	)
-})
+##################
 
 
+## connect to graph db
+graph <- startGraph(Sys.getenv("NEO_4J_HTTP")) # e.g. "http://localhost:7474"
+## load preferences (primarily how to define each node's "primary category";
+## e.g. from [biolink:NamedThing, biolink:BiologicalEntity, biolink:Gene, biolink:PhysicalEssenceOrOccurrent], prefer biolink:Gene)
+prefs <- yaml::read_yaml("meta/kg_prefs.yaml")
 
-
-library(tidygraph)
-
-get_neighborhood <- function(id = "MONDO:0009061") {
-	res <- neo2R::cypher(graph,
-											prepCql('MATCH (n) return n limit 3'),
-											#parameters = list(id = id),
-											result = "graph")
-
-
-	# Assuming 'your_list' is the list structure you provided
-	nodes <- res$nodes
-	nodes_df <- data.frame(id = lapply(nodes, function(node) { node$properties$id }))
-	nodes_df$category <- lapply(nodes, function(node) { node$ })
-
-}
-
-x <- get_neighborhood()
-
-library(tidygraph)
-rstat_nodes <- data.frame(name = c("Hadley", "David", "Romain", "Julia"))
-rstat_edges <- data.frame(from = c(1, 1, 1, 2, 3, 3, 4, 4, 4),
-													to = c(2, 3, 4, 1, 1, 2, 1, 2, 3))
-x <- tbl_graph(nodes = rstat_nodes, edges = rstat_edges)
-
-rstat_nodes <- data.frame(name = c("Jim", "David", "John", "Rex"), age = c(23, 24, 25, 26))
-rstat_edges <- data.frame(from = c(1, 1, 1, 2, 3, 3, 4, 4, 4),
-													to = c(2, 3, 4, 1, 1, 2, 1, 2, 3))
-y <- tbl_graph(nodes = rstat_nodes, edges = rstat_edges)
-
-z <- tidygraph::graph_join(x, y)
-
-
-
-
-# calls tidygraph::as_tbl_graph on the input, ensures the output conforms to kgx format,
-# and sets required tidygraph "from" and "to" columns from "subject" and "object"
-as_tbl_kgx <- function(...) {
-	g <- tidygraph::as_tbl_graph(..., node_key = "id")
-	#str(g)
-	print(g)
-}
-
-node_df <- data.frame(id = c("MONDO:1", "MONDO:2", "MONDO:3"))
-node_df$category <- list(c(1, 2, 3), c(3, 4), c(5, 6, 7))
-edge_df <- data.frame(subject = c("MONDO:1", "MONDO:2", "MONDO:3"),
-											predicate = c("likes", "loves", "hates"),
-											object = c("MONDO:3", "MONDO:1", "MONDO:2"))
-
-
-
+## a tbl_kgx is a tidygraph::tbl_graph, conforming to the kgx specification, https://github.com/biolink/kgx/blob/master/specification/kgx-format.md
+## the from and to information defining edge endpoints is set from edge subject and object
+## and the node_key is assumed to be id
 tbl_kgx <- function(nodes = NULL, edges = NULL, ...) {
 	if(is.null(nodes$id)) { stop("Error: tbl_kgx nodes must have an 'id' column.") }
 	if(is.null(nodes$id)) { stop("Error: tbl_kgx nodes must have an 'category' column.") }
@@ -101,18 +35,6 @@ tbl_kgx <- function(nodes = NULL, edges = NULL, ...) {
 	return(g)
 }
 
-z <- tbl_kgx(nodes = node_df, edges = edge_df)
-
-library(ggraph)
-
-p <- ggraph::autograph(z)
-
-
-##################
-
-library(neo2R)
-graph <- startGraph("http://24.144.94.219:7474")
-prefs <- yaml::read_yaml("meta/kg_prefs.yaml")
 
 
 
@@ -137,28 +59,49 @@ normalize_categories <- function(cats_list, cats_prefs) {
 	return(normed)
 }
 
+## Neo4j array results come back as lists of length-1 character vectors;
+## these need to be stitched together to length-N character vectors.
+## We leave these in a list element to distinguish sets of size 1 from
+## scalar strings
+stitch_vectors <- function(x) {
+	# Check if the element is a list
+	if (is.list(x)) {
+		# Check if all elements of the list are single-character vectors
+		if (all(sapply(x, function(y) is.character(y) && length(y) == 1))) {
+			# Concatenate all single-character vectors and put them in a list
+			return(list(unlist(x)))
+		} else {
+			# Recursively apply the function to each element of the list
+			return(lapply(x, stitch_vectors))
+		}
+	} else {
+		# Return the element as is if it's not a list
+		return(x)
+	}
+}
 
-query_node_ids <- function(ids) {
-	res <- neo2R::cypher(graph,
-											 query = "MATCH (n) WHERE n.id IN $ids RETURN n",
-											 parameters = list(ids = ids),
-											 result = "graph")
+## Given a cypher query, returns a tbl_kgx graph.
+query_graph <- function(graph, query = "MATCH (s) -[p]- (o) return s, p, o LIMIT 2", parameters = NULL) {
+	res <- neo2R::cypher(graph, query= query, parameters = parameters, result = "graph")
+	res <- stitch_vectors(res)
 
-	#nodes_df <- data.frame
+	#str(res)
+	## node info
 	node_ids <- unlist(lapply(res$nodes, function(node) {
 		node$properties$id
 	}))
 
 	node_categories <- lapply(res$nodes, function(node) {
-		node$properties$category
+		node$properties$category[[1]] ## pull these out of the list container to get a simple list of vecs
 	})
 
 	nodes_df <- tibble::tibble(id = node_ids, category = node_categories)
 
-	nodes_df$pcategory <- normalize_categories(nodes_df$category, prefs$monarch_kg$category_priority)
+	## compute a pcategory, or priority category, based on a preference list
+	nodes_df$pcategory <- normalize_categories(node_categories, prefs$monarch_kg$category_priority)
 	nodes_df$selected <- FALSE
 
-
+	## add all other node properties as columns
 	node_prop_names <- unname(unique(unlist(lapply(res$nodes, function(node){
 		names(node$properties)
 	}))))
@@ -171,10 +114,64 @@ query_node_ids <- function(ids) {
 		})
 	}
 
-	g <- tbl_kgx(nodes_df)
+	if(is.null(res$relationships[[1]])) {
+		return(tbl_kgx(nodes_df))
+	}
+
+	## edge info
+	edge_subjects <- unlist(lapply(res$relationships, function(relationship) {
+		relationship$properties$subject
+	}))
+
+	edge_predicates <- unlist(lapply(res$relationships, function(relationship) {
+		relationship$properties$predicate
+	}))
+
+	edge_objects <- unlist(lapply(res$relationships, function(relationship) {
+		relationship$properties$object
+	}))
+
+	edges_df <- data.frame(subject = edge_subjects,
+												 predicate = edge_predicates,
+												 object = edge_objects)
+
+	# add all other edge properties as columns
+	edge_prop_names <- unname(unique(unlist(lapply(res$relationships, function(edge){
+		names(edge$properties)
+	}))))
+	edge_prop_names <- edge_prop_names[!edge_prop_names %in% c("subject", "predicate", "object")]
+
+	for(prop_name in edge_prop_names) {
+		# sapply!
+		edges_df[[prop_name]] <- sapply(res$relationships, function(edge) {
+			edge$properties[[prop_name]]
+		})
+	}
+
+	# set from and to info for graph
+	edges_df$from <- edge_subjects
+	edges_df$to <- edge_objects
+
+	g <- tbl_kgx(nodes_df, edges_df)
 	return(g)
 }
-r <- query_node_ids(c("MONDO:0009061", "MONDO:0005413"))
+query_graph(graph, query = "MATCH (s) -[r]- (o) return s LIMIT 1")
+query_graph(graph, query = "MATCH (s) -[r]- (o) return r LIMIT 1")
+query_graph(graph, query = "MATCH (s) -[r]- (o) return s, o LIMIT 1")
+
+query_graph(graph, query = "MATCH path = (startNode)-[*3..3]-(endNode), path2 = (startNode)-[*3..3]-(endNode) return path, path2 LIMIT 1")
+
+
+query_node_ids <- function(graph, ids) {
+	return(query_graph(graph,
+										 query = "MATCH (n) WHERE n.id IN $ids RETURN n",
+										 parameters = list(ids = ids))
+				 )
+}
+query_node_ids(graph, c("MONDO:0009061", "MONDO:0005413"))
+
+
+
 
 count_labels <- function() {
 	query <- "
@@ -191,17 +188,6 @@ ORDER BY usageCount DESC
 count_labels()
 
 
-
-# system.time({
-# 	df <- cypher(
-# 		graph,
-# 		prepCql(
-# 			'MATCH (n)-[r]-(p) return n,r,p limit 1'
-# 		),
-# 		result = "graph"
-# 	)
-# })
-#
 
 
 
