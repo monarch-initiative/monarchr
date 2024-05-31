ef41da5fcf7afbe47b1e304805be3679b219aa21
Shawn T O'Neil <oneilsh@gmail.com>
2024-05-31 15:38:08 -0700

    Move normalize_categories to separate file (shared private function)

--- R/cypher_query.neo4j_engine.R
+++ R/cypher_query.neo4j_engine.R
@@ -1,35 +1,5 @@
 ########### Private functions ###########
 
-#' Normalize Categories
-#'
-#' This function takes a list of vectors of categories and an ordered preference list over categories.
-#' It selects the most preferred category from each vector, or the first category if no preferred categories are included.
-#'
-#' @param cats_list A list of vectors of categories.
-#' @param cats_prefs An ordered preference list over categories.
-#' @return A vector of normalized categories.
-#' @examples
-#' categories_list <- list(c("A", "B", "C"), c("D", "E", "F"))
-#' categories_prefs <- c("B", "E", "A", "D", "C", "F")
-#' normalize_categories(categories_list, categories_prefs)
-normalize_categories <- function(cats_list, cats_prefs) {
-	normed <- unlist(lapply(cats_list, function(categories) {
-		positions <- match(categories, cats_prefs)
-
-		# If all matches are NO, there is no preference match, so use the first label;
-		# otherwise, use the label with the minimum position in the preferred list
-		most_preferred_label <- if (all(is.na(positions))) {
-			categories[1]
-		} else {
-			categories[which.min(positions)]
-		}
-
-		most_preferred_label
-	}))
-
-	return(normed)
-}
-
 
 #' Stitch Vectors
 #'
