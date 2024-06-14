d600f2e97898b681c8b8f58a772d898dde941cb6
Shawn T O'Neil <oneilsh@gmail.com>
2024-06-14 15:22:52 -0700

    fix fetch_nodes.file_engine to return no edges for compatibility with neo4j engine

--- R/fetch_nodes.file_engine.R
+++ R/fetch_nodes.file_engine.R
@@ -1,28 +1,22 @@
-
-#' Fetch nodes from a graph
-#' 
-#' This function fetches nodes from a graph based on a set of conditions.
-#' 
-#' @param engine A graph engine object
-#' @param ... A set of conditions identifying the nodes to fetch, only used if query_ids is NULL
-#' @param query_ids A character vector of identifiers to fetch
-#' @return A tbl_kgx object containing the nodes
-#' @examples
-#' e <- file_engine(system.file("extdata", "mondo_kgx_tsv.tar.gz", package = "monarchr"))
-#' g <- fetch_nodes(e, id == c("MONDO:0007525", "MONDO:0007526"))
-#' g <- fetch_nodes(e, "biolink:Disease" %in% category | "biolink:Gene" %in% category)
-#' g <- fetch_nodes(e, "biolink:Disease" %in% category, limit = 5)
 #' @export
+#' @import tidygraph
+#' @import dplyr
 fetch_nodes.file_engine <- function(engine, ..., query_ids = NULL) {
     if(!is.null(query_ids)) {
-        res <- engine$graph %>% 
-            activate(nodes) %>% 
+        res <- engine$graph %>%
+            activate(nodes) %>%
             filter(id %in% query_ids)
     } else {
-        res <- engine$graph %>% 
-            activate(nodes) %>% 
+        res <- engine$graph %>%
+            activate(nodes) %>%
             filter(...)
     }
+
+    # we want to drop all the edges to be compatible with the neo4j engine
+    res <- res %>%
+        activate(edges) %>%
+        filter(FALSE) %>%
+        activate(nodes)
 
     attr(res, "last_engine") <- engine
 
