e934e55b52122cb1f6566c079c25d53d47fb8051
Shawn T O'Neil <oneilsh@gmail.com>
2024-06-18 11:52:31 -0700

    rmd print WIP

--- R/print.tbl_kgx.R
+++ R/print.tbl_kgx.R
@@ -53,3 +53,78 @@ print.tbl_kgx <- function(x, suppress_prefix = NULL, ...) {
     print(edf)
 }
 
+
+order_nodes <- function(g) {
+	e <- attr(g, "last_engine")
+	node_prefs <- e$preferences$node_property_priority
+	edge_prefs <- e$preferences$edge_property_priority
+
+	current_node_names <- colnames(nodes(g))
+	used_prefs_node_names <- node_prefs[node_prefs %in% current_node_names]
+	set_node_names <- c(used_prefs_node_names, setdiff(current_node_names, used_prefs_node_names))
+
+	current_edge_names <- colnames(edges(g))
+	used_prefs_edge_names <- edge_prefs[edge_prefs %in% current_edge_names]
+	set_edge_names <- c(used_prefs_edge_names, setdiff(current_edge_names, used_prefs_edge_names))
+
+	res <- g |>
+		activate(nodes) |>
+		select(set_node_names) |>
+		activate(edges) |>
+		select(set_edge_names) |>
+		activate(nodes)
+
+	return(res)
+}
+
+
+
+#' @import kableExtra
+#' @import stringr
+clean_df <- function(df) {
+	new_df <- list()
+	for(colname in colnames(df)) {
+		col_i <- df[[colname]]
+		if(is.list(col_i)) {
+			# list col
+			colname <- paste0(colname, " (list)")
+
+		}
+		contents <- lapply(col_i, function(cell_data) {
+			paste(capture.output(dput(cell_data, control = "useSource")), collapse = "")
+			#paste(cell_data, collapse = "")
+		}) |> unlist()
+
+		#contents_trunc <- str_trunc(contents, 60)
+		#new_df[[colname]] <- cell_spec(contents_trunc, "html", tooltip = contents)
+		new_df[[colname]] <- contents
+	}
+
+	as_tibble(new_df)
+}
+
+
+
+#' @export
+#' @import knitr
+knit_print.tbl_kgx <- function(graph, ...) {
+	g <- order_nodes(graph)
+	nodes_kbl <- clean_df(nodes(g)) |> kable("html", escape = FALSE) |> kable_styling()
+	edges_kbl <- clean_df(edges(g)) |> kable("html", escape = FALSE) |> kable_styling()
+	knitr::asis_output(knitr::knit_child(text = c(
+		'### {.tabset}',
+		'',
+		'#### Nodes',
+		'<div style="max-height: 400px;overflow-y: auto;border-left: 1px solid #ddd;border-right:  1px solid #ddd;border-bottom: 1px solid #ddd;">',
+		'```{r eval=TRUE, echo=FALSE}',
+		'nodes_kbl',
+		'```',
+		'</div>',
+		'#### Edges',
+		'<div style="max-height: 400px;overflow-y: auto;border-left: 1px solid #ddd;border-right:  1px solid #ddd;border-bottom: 1px solid #ddd;">',
+		'```{r eval=TRUE, echo=FALSE}',
+		'edges_kbl',
+		'```',
+		'</div>'
+	), envir = environment(), quiet = TRUE))
+}
