18d782eda11692a1fbb72a6defa2c2f0b80e5dc3
Shawn T O'Neil <oneilsh@gmail.com>
2024-06-21 12:53:56 -0700

    fix bug in search_nodes.monarch_engine to allow multiple categories filter

--- R/search_nodes.monarch_engine.R
+++ R/search_nodes.monarch_engine.R
@@ -1,3 +1,25 @@
+# for array params in httr queries
+# from https://stackoverflow.com/a/72532186
+flattenbody <- function(x) {
+	# A form/query can only have one value per name, so take
+	# any values that contain vectors length >1 and
+	# split them up
+	# list(x=1:2, y="a") becomes list(x=1, x=2, y="a")
+	if (all(lengths(x)<=1)) return(x);
+	do.call("c", mapply(function(name, val) {
+		if (length(val)==1 || any(c("form_file", "form_data") %in% class(val))) {
+			x <- list(val)
+			names(x) <- name
+			x
+		} else {
+			x <- as.list(val)
+			names(x) <- rep(name, length(val))
+			x
+		}
+	}, names(x), x, USE.NAMES = FALSE, SIMPLIFY = FALSE))
+}
+
+
 #' @export
 #' @import tidygraph
 #' @import dplyr
@@ -25,7 +47,7 @@ search_nodes.monarch_engine <- function(engine,
     }
 
     # put the httr::GET call in a trycatch block to handle errors
-    response <- GET(api_url, query = params)
+    response <- GET(api_url, query = flattenbody(params))
 
     # if the response is not 200, throw an error
     if(response$status_code != 200) {
