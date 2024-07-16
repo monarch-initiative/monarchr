7cd62622d0b1e13e63c9d6d366fd1f1ecf4d562a
Shawn T O'Neil <oneilsh@gmail.com>
2024-07-16 10:31:45 -0400

    remove search_nodes for file_engine and neo4j engine in favor of fetch_nodes

--- R/monarch_search.R
+++ R/monarch_search.R
@@ -19,18 +19,32 @@ flattenbody <- function(x) {
 	}, names(x), x, USE.NAMES = FALSE, SIMPLIFY = FALSE))
 }
 
-
+#' Search for KG nodes using the Monarch Initiative search API
+#'
+#' This function is a wrapper around the Monarch-hosted
+#' [search API](https://api.monarchinitiative.org/v3/docs#/search/search_v3_api_search_get).
+#' It returns nodes (no edges) from the Monarch KG, fetched via an instance of `monarch_engine()`.
+#'
+#' @param query Search query string, e.g. "Cystic fibrosis"
+#' @param category A set of node category labels to limit the search to, e.g. c("biolink:Disease", "biolink:Gene")
+#' @param limit Maximum number of nodes to return. Default 10.
+#' @param ... Other parameters (unused).
+#' @return A local tbl_kgx graph with no edges.
 #' @export
 #' @import tidygraph
 #' @import dplyr
 #' @importFrom assertthat assert_that
 #' @importFrom httr GET content http_status
-search_nodes.monarch_engine <- function(engine,
-                                     query,
-                                     category = NULL,
-                                     limit = 10,
-                                     ...) {
+#' @examplesIf monarch_engine_check()
+#' cf_hits <- monarch_search("Cystic fibrosis", category = "biolink:Disease", limit = 5)
+#' print(cf_hits)
+#'
+monarch_search <- function(query,
+                           category = NULL,
+                           limit = 10,
+                           ...) {
 
+		engine <- monarch_engine()
     api_url <- paste0(engine$preferences$monarch_api_url, "/search")
 
     # ensure that the limit is not null and is a length-1 integer <= 500
