f265f4d6fca77c649714bb194ddc5cf5522aeff4
Shawn T O'Neil <oneilsh@gmail.com>
2024-07-18 13:10:34 -0500

    pull flatten body for httr out of monarch search file

--- R/monarch_search.R
+++ R/monarch_search.R
@@ -1,23 +1,3 @@
-# for array params in httr queries
-# from https://stackoverflow.com/a/72532186
-flattenbody <- function(x) {
-	# A form/query can only have one value per name, so take
-	# any values that contain vectors length >1 and
-	# split them up
-	# list(x=1:2, y="a") becomes list(x=1, x=2, y="a")
-	if (all(lengths(x)<=1)) return(x);
-	do.call("c", mapply(function(name, val) {
-		if (length(val)==1 || any(c("form_file", "form_data") %in% class(val))) {
-			x <- list(val)
-			names(x) <- name
-			x
-		} else {
-			x <- as.list(val)
-			names(x) <- rep(name, length(val))
-			x
-		}
-	}, names(x), x, USE.NAMES = FALSE, SIMPLIFY = FALSE))
-}
 
 #' Search for KG nodes using the Monarch Initiative search API
 #'
@@ -61,7 +41,7 @@ monarch_search <- function(query,
     }
 
     # put the httr::GET call in a trycatch block to handle errors
-    response <- GET(api_url, query = flattenbody(params))
+    response <- GET(api_url, query = flatten_body_for_httr(params))
 
     # if the response is not 200, throw an error
     if(response$status_code != 200) {
