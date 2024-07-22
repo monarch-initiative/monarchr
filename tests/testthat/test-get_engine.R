e306f7a60c5cc97481c620491f9ddccb230e6c5c
Shawn T O'Neil <oneilsh@gmail.com>
2024-07-22 10:13:24 -0700

    fix get_engine test

--- tests/testthat/test-get_engine.R
+++ tests/testthat/test-get_engine.R
@@ -4,8 +4,8 @@ library(assertthat)
 test_that("get_engine works", {
     # skip for now
     #testthat::skip("temporary skip")
-    
-    g <- monarch_engine() %>% search_nodes("fanconi anemia", limit = 5)
+
+    g <- monarch_search("fanconi anemia", limit = 5)
     engine <- get_engine(g)
 
     expect_s3_class(engine, "neo4j_engine")
