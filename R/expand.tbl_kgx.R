9e0cd647659f04540296ba7f20eac59b6002d1ff
Shawn T O'Neil <oneilsh@gmail.com>
2025-06-04 17:29:30 -0400

    fixes for active status and transitive_closure w/ no transitive edges

--- R/expand.tbl_kgx.R
+++ R/expand.tbl_kgx.R
@@ -4,16 +4,19 @@
 #' @importFrom assertthat assert_that
 expand.tbl_kgx <- function(graph, ...) {
     # check to see if g has a last_engine attribute
+	  active_tbl <- active(graph)
     if(!is.null(attr(graph, "last_engine"))) {
         engine <- attr(graph, "last_engine")
         if(any(c("monarch_engine", "neo4j_engine") %in% class(engine))) {
-        	return(expand_neo4j_engine(engine, graph, ...))
+        	res <- expand_neo4j_engine(engine, graph, ...) |> activate(!!rlang::sym(active_tbl))
+        	return(res)
         } else if("file_engine" %in% class(engine)) {
-        	return(expand_file_engine(engine, graph, ...))
+        	res <- expand_file_engine(engine, graph, ...) |> activate(!!rlang::sym(active_tbl))
+        	return(res)
         } else {
         	stop("Error: unknown or incompatible engine.")
         }
-        return(expand(engine, graph, ...))
+        # return(expand(engine, graph, ...)) # this shouldn't be reachable
     } else {
         stop("Error: tbl_kgx object does not have a most recent engine.")
     }
