65b702a5f8e2cc7140d3164009b04699379f7f74
Shawn T O'Neil <oneilsh@gmail.com>
2025-08-08 08:48:31 -0400

    typo fix in docs

---
+++ vignettes/monarchr.R
@@ -0,0 +1,8 @@
+## ----include=FALSE, message=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+library(dplyr)
+library(tidygraph)
+library(monarchr)
+
+options(width = 300)
+knitr::opts_chunk$set(eval = TRUE, echo = TRUE, fig.width = 10, message = FALSE, warning = FALSE)
+
