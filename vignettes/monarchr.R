026f331d66fbf59b43524568e4eeb0d181316a6c
Shawn T O'Neil <oneilsh@gmail.com>
2025-08-13 19:18:20 -0400

    minor vignette fixes

--- vignettes/monarchr.R
+++
@@ -1,8 +0,0 @@
-## ----include=FALSE, message=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-library(dplyr)
-library(tidygraph)
-library(monarchr)
-
-options(width = 300)
-knitr::opts_chunk$set(eval = TRUE, echo = TRUE, fig.width = 10, message = FALSE, warning = FALSE)
-
